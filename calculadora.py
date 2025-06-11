2114a61a423727c31cf8b07c49d4f9734c995650
Nelson <nrcalle_790@hotmail.com>
2025-06-11 17:12:48 -0500

    fe

--- calculadora.py
+++ calculadora.py
@@ -10,7 +10,7 @@ CORS(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("calculadora-basica.html")
 
 @app.route("/calcular", methods=["POST"])
 def calcular():
